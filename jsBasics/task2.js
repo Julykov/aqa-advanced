8c80dae9c2325bfe63cf1a420b7ef3be73fe865c
Yuliia <julynewbox@gmail.com>
2025-11-14 16:13:12 +0200

    Added scripts for lesson 4.1

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let name1 = "Jack"
+let name2 = "Mary"
+
+let greeting = "Hi " + name1 + " and " + name2 + ". How are you doing?"
+console.log(greeting)
+
+let greeting2 = `Hi ${name1} and ${name2}. How are you doing?`
+console.log(greeting2)
---
+++ jsBasics/task4_3.js
@@ -0,0 +1,6 @@
+let radius = 3
+let height = 5
+let volume = Math.PI * Math.pow(radius, 2) * height
+let rounded_volume = Math.round(volume * 100) / 100
+
+console.log(rounded_volume)
