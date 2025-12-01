08ef4ca6e18c26c733415000bf02ea5f090b05fd
Yuliia <julynewbox@gmail.com>
2025-12-01 21:57:54 +0200

    Scripts added for lesson 8_1

---
+++ jsBasics/task8_2.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5]
+
+function changeArr(nums) {
+    return nums.map((num, index) => num*index)
+}
+
+const numbers2 = changeArr(numbers)
+console.log(numbers2)
---
+++ jsBasics/task8_6.js
@@ -0,0 +1,10 @@
+const numbersList = [1,10,14,2,4,5,43,34]
+
+const secondArr = numbersList.slice()
+const sortedSecondArr = secondArr.sort((a,b) => a-b)
+
+console.log(numbersList)
+console.log(sortedSecondArr)
+
+console.log(`\nFirst array: ${numbersList}`)
+console.log(`Second array: ${sortedSecondArr}`)
