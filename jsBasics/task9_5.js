26c0c2d394347adc7127d33d71fc78a7f0b3bfe8
Yuliia <julynewbox@gmail.com>
2025-12-07 21:20:55 +0200

    Script is corrected

--- jsBasics/task9_5.js
+++ jsBasics/task9_5.js
@@ -4,14 +4,6 @@ const users = [
     {"name": "Bill", "email": "some_bill_email@gmail.com", "age": 20}
 ]
 
-for (const user of users) {
-    const {name, email, age} = user
-    console.log(name)
-    console.log(email)
-    console.log(age)
+for (const {name, email, age} of users) {
+    console.log(`The user with name ${name} and email ${email} is ${age} years old`)
 }  
-
-for (const user of users) {
-    const entries = Object.entries(user)
-    console.log(entries)
-}   
